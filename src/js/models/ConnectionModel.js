83d0f3304a6f2ff23847ce6f125ae5e43da651b5
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-06-27 16:55:38 -0500

    Add more ConnectionModel accessors: user, password, host, port

--- src/js/models/ConnectionModel.js
+++ src/js/models/ConnectionModel.js
@@ -8,6 +8,42 @@ maria.Model.subclass(coupler, 'ConnectionModel', {
         this._adapter = adapter;
         this.dispatchEvent({type: 'change'});
       }
+    },
+    getUser: function() {
+      return this._user;
+    },
+    setUser: function(user) {
+      if (this._user !== user) {
+        this._user = user;
+        this.dispatchEvent({type: 'change'});
+      }
+    },
+    getPassword: function() {
+      return this._password;
+    },
+    setPassword: function(password) {
+      if (this._password !== password) {
+        this._password = password;
+        this.dispatchEvent({type: 'change'});
+      }
+    },
+    getHost: function() {
+      return this._host;
+    },
+    setHost: function(host) {
+      if (this._host !== host) {
+        this._host = host;
+        this.dispatchEvent({type: 'change'});
+      }
+    },
+    getPort: function() {
+      return this._port;
+    },
+    setPort: function(port) {
+      if (this._port !== port) {
+        this._port = port;
+        this.dispatchEvent({type: 'change'});
+      }
     }
   }
 });
--- test/js/test_ConnectionModel.js
+++ test/js/test_ConnectionModel.js
@@ -16,4 +16,76 @@ suite('ConnectionModel', function() {
       assert(callback);
     });
   });
+
+  suite('#setUser', function() {
+    test('should set user', function() {
+      var connection = new coupler.ConnectionModel();
+      connection.setUser('foo');
+      assert.equal('foo', connection.getUser());
+    });
+
+    test('should dispatch change event', function() {
+      var connection = new coupler.ConnectionModel();
+      var callback = false;
+      maria.on(connection, 'change', function() {
+        callback = true;
+      });
+      connection.setUser('foo');
+      assert(callback);
+    });
+  });
+
+  suite('#setPassword', function() {
+    test('should set password', function() {
+      var connection = new coupler.ConnectionModel();
+      connection.setPassword('foo');
+      assert.equal('foo', connection.getPassword());
+    });
+
+    test('should dispatch change event', function() {
+      var connection = new coupler.ConnectionModel();
+      var callback = false;
+      maria.on(connection, 'change', function() {
+        callback = true;
+      });
+      connection.setPassword('foo');
+      assert(callback);
+    });
+  });
+
+  suite('#setHost', function() {
+    test('should set host', function() {
+      var connection = new coupler.ConnectionModel();
+      connection.setHost('foo');
+      assert.equal('foo', connection.getHost());
+    });
+
+    test('should dispatch change event', function() {
+      var connection = new coupler.ConnectionModel();
+      var callback = false;
+      maria.on(connection, 'change', function() {
+        callback = true;
+      });
+      connection.setHost('foo');
+      assert(callback);
+    });
+  });
+
+  suite('#setPort', function() {
+    test('should set port', function() {
+      var connection = new coupler.ConnectionModel();
+      connection.setPort('foo');
+      assert.equal('foo', connection.getPort());
+    });
+
+    test('should dispatch change event', function() {
+      var connection = new coupler.ConnectionModel();
+      var callback = false;
+      maria.on(connection, 'change', function() {
+        callback = true;
+      });
+      connection.setPort('foo');
+      assert(callback);
+    });
+  });
 });
