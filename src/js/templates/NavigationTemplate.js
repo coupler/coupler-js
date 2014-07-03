d92b3d2ed58b45334012d3937245b1abfb9fb8b3
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-07-03 16:37:21 -0500

    Create Navigation{Template,View}

---
+++ src/js/templates/NavigationTemplate.js
@@ -0,0 +1,1 @@
+coupler.NavigationTemplate = '<div class="navigation"><ul><li class="connections">Connections</li></ul></div>';
---
+++ src/js/views/NavigationView.js
@@ -0,0 +1,2 @@
+maria.ElementView.subclass(coupler, 'NavigationView', {
+});
