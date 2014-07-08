3f52bd0e6f0b2ccf1cbfb5174b6ae71d20299da2
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-07-08 16:45:07 -0500

    Use persistence in application

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,5 +1,10 @@
 (function() {
+  var storage = new coupler.StorageModel();
+  storage.setMode('object', window.localStorage);
+
   var app = new coupler.AppModel();
+  storage.add(app, 'app');
+
   var view = new coupler.AppView(app);
   document.body.appendChild(view.build());
 })();
